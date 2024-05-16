8220db49756e8d729b0ef29669768504c085550f
AlanDAmario <vega93@live.it>
2024-05-16 16:37:30 +0200

    check

--- main.js
+++ main.js
@@ -31,11 +31,27 @@ const bikes = [
     },
 ]
 
-const filterWeight = bikes.filter((lowWeight) => {
-    if (lowWeight.weight < 42) {
-        return true
+//TROVIAMO IL PESO MINORE ATTRAVERSO MATH.MIN
+const minWeight = Math.min(...bikes.map(weight => weight.peso));
+
+//FILTRIAMO GLI OGGETTI 
+const findMinWeight = bikes.filter(weight => weight.peso === minWeight);
+
+//STAMPIAMO IL PESO MINIMO
+findMinWeight.forEach(weight => {
+    console.log(weight.weight);
+})
+
+// Snack2
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà:
+// Punti fatti e falli subiti.
+// Infine  creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+const team = [
+    {
+
     }
-})
-filterWeight.forEach(lowWeight => {
-    console.log(`${lowWeight.weight}kg`);
-})
+]
